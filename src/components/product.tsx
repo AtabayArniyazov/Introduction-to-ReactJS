b5000862615ab85a76d031f02148b267a4800bf9
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-01-25 16:35:36 +0300

    [ReactJs] --- use useState for show or hide button (like a *ngIf);

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,14 +1,32 @@
 import { IProduct } from "../models";
+import { useState } from "react";
 
 interface ProductProps {
     product: IProduct
 }
 export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false);
+
     return (
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
             <img src={product.image} className="w-1/6" alt={product.title}></img>
             <p>{ product.title }</p>
             <p className="font-bold">{ product.price }</p>
+            {!details && <button
+                className="py-2 px-4 border bg-yellow-400"
+                onClick={() => setDetails(true)}>
+                Show details
+            </button>}
+            {details && <button
+                className="py-2 px-4 border bg-blue-400"
+                onClick={() => setDetails(false)}>
+                Hide details
+            </button>}
+            {details && <div>
+                <p>
+                    {product.description}
+                </p>
+            </div>}
         </div>
     )
 }

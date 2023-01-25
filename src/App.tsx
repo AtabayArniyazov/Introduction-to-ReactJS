f90e85b41db766366b7558d41700081ed5ebe714
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-01-25 15:50:01 +0300

    [ReactJs] --- try without JSX;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React, {createElement as e} from 'react';
+import React, {createElement as e, useState} from 'react';
 
 function App() {
-  // return (
-  //     <h1>Hello React!!!</h1>
-  // );
-  return e('div', {className: 'container'}, [
-      e('h1', {className: 'font-bold', key: 1}, 'Test JSX'),
-      e('button', {className: '', key: 2}, 'Click Me!')
-  ])
+    const [count, setCount] = useState(0);
+
+    return e('div', {className: 'container'}, [
+        e('h1', {className: 'font-bold', key: 1}, `Test JSX ${count}`),
+        e('button', {
+            className: 'py-2 px-4 border',
+            key: 2,
+            onClick: () => setCount(count + 1)
+        }, 'Click Me!')
+    ]);
 }
 
 export default App;

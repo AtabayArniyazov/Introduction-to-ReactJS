2ce1f97a5956cce0c6456e6524dd7b7318a6e545
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-01-31 13:59:13 +0300

    [ReactJs] --- add 'loading' indicator;

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import {IProduct} from "./models";
 
 function App() {
     const [products, setProducts] = useState<IProduct[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
 
     async function fetchProducts(limit = 5) {
+        setLoading(true);
+
         return await axios.get<IProduct[]>(`https://fakestoreapi.com/products?limit=${limit}`);
     }
 
     useEffect(() => {
-        fetchProducts().then((response) => setProducts(response.data));
+        fetchProducts()
+            .then((response) => setProducts(response.data))
+            .finally(() => setLoading(false));
     }, []);
 
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            { products.map((product) => <Product product={product} key={product.id} />) }
+            { loading && <p className="text-center">Loading...</p> }
+            { !loading &&  products.map((product) => <Product product={product} key={product.id} />) }
         </div>
     );
 }

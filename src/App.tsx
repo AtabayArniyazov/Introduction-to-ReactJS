8c5ddb033ee98213b8e96c8e3ce806be71eb67a7
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-01-31 13:46:20 +0300

    [ReactJs] --- load data from the server; before it was static data;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { Product } from "./components/product";
-import { products } from "./data/products";
+import React, {useEffect, useState} from 'react';
+import {Product} from "./components/product";
+import axios from "axios";
+import {IProduct} from "./models";
 
 function App() {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    async function fetchProducts(limit = 5) {
+        return await axios.get<IProduct[]>(`https://fakestoreapi.com/products?limit=${limit}`);
+    }
+
+    useEffect(() => {
+        fetchProducts().then((response) => setProducts(response.data));
+    }, []);
+
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-
             { products.map((product) => <Product product={product} key={product.id} />) }
         </div>
     );

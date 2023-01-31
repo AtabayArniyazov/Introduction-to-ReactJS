a4753c44b9274e544cf73c65c62d4cda0d113b60
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-01-31 15:15:49 +0300

    [ReactJs] --- move error to separate component;

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Product } from "./components/product";
 import { useProducts } from "./hooks/products";
 import { Loader } from "./components/loader";
+import { ErrorMessage } from "./components/errorMessage";
 
 function App() {
     const { products, loading, error } = useProducts();
@@ -9,8 +10,8 @@ function App() {
 
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            { loading && Loader() }
-            { error && <p className="text-center text-red-500">{ error }</p> }
+            { loading && <Loader /> }
+            { error &&  <ErrorMessage error={error} />}
             { !loading &&  products.map((product) => <Product product={product} key={product.id} />) }
         </div>
     );

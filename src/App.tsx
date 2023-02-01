e94deea21dcb112f7944e7bf0274729ba8c6aa7a
Atabay Arniyazov <Atabaj.Arniyazov@p97.com>
2023-02-01 17:34:27 +0300

    [ReactJs] --- add navigation;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { ProductPage } from "./pages/productPage";
 import { AboutPage } from "./pages/aboutPage";
+import { Navigation } from "./components/navigation";
 
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={ <ProductPage/> }/>
-            <Route path="/about" element={ <AboutPage/> }/>
-        </Routes>
+        <>
+            <Navigation></Navigation>
+            <Routes>
+                <Route path="/" element={ <ProductPage/> }/>
+                <Route path="/about" element={ <AboutPage/> }/>
+            </Routes>
+        </>
     );
 }
 
